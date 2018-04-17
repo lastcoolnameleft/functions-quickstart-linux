ccb9e7754b324da8284dbc2d6a77c80ff4b7d63d
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-04-17 12:37:33 -0500

    Update index.js

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "HI2! " + (req.query.name || req.body.name)
+            body: "HI3! " + (req.query.name || req.body.name)
         };
     }
     else {
